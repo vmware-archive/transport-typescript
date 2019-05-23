1a3f774e12f183962c91c75ea6c81ac5f826aee7
Dave Shanley <dshanley@vmware.com>
2019-05-23 17:08:07 -0400

    Bifrost 0.14.9
    Fixed small glitch in Transactions that created a leak when errors were encountered. Error stream was not being cleaned up correctly.

--- src/bus.api.ts
+++ src/bus.api.ts
@@ -18,7 +18,7 @@ import { FabricApi } from './fabric.api';
 import { BrokerConnector } from './bridge';
 
 // current version
-const version = '0.14.8';
+const version = '0.14.9';
 
 export type ChannelName = string;
 export type SentFrom = string;
