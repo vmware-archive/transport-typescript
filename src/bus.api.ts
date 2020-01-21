9188cf008db41d9e2448792199ee14e01968ca9a
Josh Kim <kjosh@vmware.com>
2020-01-21 11:26:11 -0800

    Fix issue with global HTTP headers overwriting order
    This MR fixes an issue associated with setGlobalHttpHeaders()
    where if entries with the same key exist on both global headers
    store as well as in the HTTP request itself, the global one
    takes precedence over what's supposed to be of higher order.

--- src/bus.api.ts
+++ src/bus.api.ts
@@ -18,7 +18,7 @@ import { FabricApi } from './fabric.api';
 import { BrokerConnector } from './bridge';
 
 // current version
-const version = '0.14.29';
+const version = '0.14.30';
 
 export type ChannelName = string;
 export type SentFrom = string;
