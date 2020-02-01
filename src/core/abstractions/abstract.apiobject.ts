eb31f4aac2f7f8e23fbdc40406444c95c0421824
Akmal Khan <akmalk@vmware.com>
2020-01-31 17:19:52 -0800

    Support for passing header parameters to Rest service. ApiObject is used to transport the API headers.

--- src/core/abstractions/abstract.apiobject.ts
+++ src/core/abstractions/abstract.apiobject.ts
@@ -2,6 +2,8 @@ import { AbstractMessageObject } from './abstract.messageobject';
 
 
 export class ApiObject<TRequestObject, TResponseObject> {
+    private apiHeaders = {};    // for API header parameters from Apigen
+
     public readonly requestObject: AbstractMessageObject<any, any>;
     public readonly responseObject: AbstractMessageObject<any, any>;
 
@@ -14,6 +16,14 @@ export class ApiObject<TRequestObject, TResponseObject> {
     constructor(requestObject: AbstractMessageObject<any, any>, responseObject: AbstractMessageObject<any, any>) {
         this.requestObject = requestObject;
         this.responseObject = responseObject;
+    }
+
+    public getHeaders(): any {
+        return this.apiHeaders;
+    }
+
+    public addHeader(key: string, value: string) {
+        this.getHeaders()[key] = value;
     }
 }
 
