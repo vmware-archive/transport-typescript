d88535fa064aa024b732be037ba35112ed9fb9bd
Deyan Valkanov <dvalkanov@vmware.com>
2021-05-10 13:13:53 +0300

    Update version and change log
    Close: #36
    
    Signed-off-by: Deyan Valkanov <dvaklanov@vmware.com>

--- src/bus.api.ts
+++ src/bus.api.ts
@@ -18,7 +18,7 @@ import { FabricApi } from './fabric.api';
 import { BrokerConnector } from './bridge';
 
 // current version
-const version = '1.2.7';
+const version = '1.2.8';
 
 export type ChannelName = string;
 export type SentFrom = string;
